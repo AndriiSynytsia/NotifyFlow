d6421d88a58c218192c54f51413d08b6c5c6ca70
Andrii Synytsia <asynytsia.work@gmail.com>
2026-06-04 21:27:50 -0600

    feat(exception): add NotificationDeliveryException for delivery failures

---
+++ notifyflow-api/src/main/java/com/notifyflow/notification/exception/NotificationDeliveryException.java
@@ -0,0 +1,11 @@
+package com.notifyflow.notification.exception;
+
+public class NotificationDeliveryException extends RuntimeException{
+    public NotificationDeliveryException(String message) {
+        super(message);
+    }
+
+    public NotificationDeliveryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
