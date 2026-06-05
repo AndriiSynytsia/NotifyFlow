e3e6f7af68383c0cd699131a35baf39988db044d
Andrii Synytsia <asynytsia.work@gmail.com>
2026-06-04 20:28:52 -0600

    fix(repository): fix Pageable parameter order and remove unused import

--- notifyflow-api/src/main/java/com/notifyflow/notification/repository/NotificationRepository.java
+++ notifyflow-api/src/main/java/com/notifyflow/notification/repository/NotificationRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     Page<Notification> findByStatus(NotificationStatus status, Pageable pageable);
 
-    Page<Notification> findByStatusAndScheduledAtLessThanEqual(Pageable pageable, NotificationStatus status, ZonedDateTime scheduledAt);
+    Page<Notification> findByStatusAndScheduledAtLessThanEqual(NotificationStatus status, ZonedDateTime scheduledAt, Pageable pageable);
 }
